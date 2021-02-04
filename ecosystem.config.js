179d2120f5f1db6b10076a66d8701bf2f59a276c
Reazul Iqbal <admin@reazul.net>
2021-02-04 09:36:34 +0600

    Moving to production

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,8 +6,8 @@ module.exports = {
     env_production: {
       TZ: 'UTC',
       NODE_ENV: 'production',
-      API_BASE_URL: 'https://predictland.dtools.dev/api',
-      WS_SERVER: 'wss://predictland.dtools.dev/ws',
+      API_BASE_URL: 'https://api.dublup.io',
+      WS_SERVER: 'wss://ws.dublup.io',
       CHAT_API: 'https://beechat.hive-engine.com/api',
       CHAT_WS: 'wss://ws.beechat.hive-engine.com',
       CHAT_CHANNELS: ['01EPB6A2PPSW0BQVJ7WDDP568C', '01EVQS8BT5X4T29CGQDGR14CXN']
